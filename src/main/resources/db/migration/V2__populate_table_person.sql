443d781233e175be41ff257485f4458cc2ba550f
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-07-10 13:46:07 -0300

    migrations com flyway

---
+++ src/main/resources/db/migration/V2__populate_table_person.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (address, first_name, gender, last_name) VALUES
+    ('São Paulo','Giulia','Female','Benito'),
+    ('Curitiba','Isabel','Female','Henrique'),
+    ('São Paulo','João','Male','Henrique'),
+    ('Recife','Maria','Female','Henrique');
