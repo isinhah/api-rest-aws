43af17d9e1487b7d811feb580229de03d30a6234
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-07-11 17:53:53 -0300

    entidade book

---
+++ src/main/java/com/spring/api_rest/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package com.spring.api_rest.repositories;
+
+import com.spring.api_rest.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {}
