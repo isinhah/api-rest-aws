662c427ee043a59b9faee180389d0fe7c80d1365
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-07-11 18:18:16 -0300

    testcontainers e rest assured

--- src/main/java/com/spring/api_rest/config/OpenApiConfig.java
+++ src/main/java/com/spring/api_rest/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("RESTFUL API with Java 17 and Spring Boot 3")
                         .version("v1")
-                        .description("Esta API permite gerenciar informações de pessoas, oferecendo operações de criação, leitura, atualização e exclusão (CRUD).")
+                        .description("Esta API permite gerenciar informações de pessoas e os livros registrados, oferecendo operações de criação, leitura, atualização e exclusão (CRUD).")
                         .license(new License().name("Apache 2.0")));
     }
 }
---
+++ src/test/java/com/spring/api_rest/configs/TestConfigs.java
@@ -0,0 +1,5 @@
+package com.spring.api_rest.configs;
+
+public class TestConfigs {
+    public static final int SERVER_PORT = 8888;
+}
